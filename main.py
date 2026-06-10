019bf723344e1e2239c635d546e10a0675a481b8
dkim2289 <dhkim.89.1497@icloud.com>
2026-06-10 22:12:54 +1000

    having print_report functionality as a separate function rather than printing the raw frame in the main function

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import sys
 from stats import word_counter, char_counter, sort_by_nums
 
-def get_book_text(filepath):
-	with open(filepath) as f:
-		return f.read()
 
 def main():
 	if len(sys.argv) < 2:
@@ -12,16 +9,31 @@ def main():
 	book_path = sys.argv[1]
 	book_text = get_book_text(book_path)
 	num_words = word_counter(book_text)
+	# next line needs to be changed as to get chars dictionary, instead of char count
 	char_count = char_counter(book_text)
+	# next line needs to sort the chars_dict instead of char_count
 	sorted_list = sort_by_nums(char_count)
-	
-	print("============ BOOKBOT ============")
-	print(f"Analyzing book found at {book_path}...")
-	print("----------- Word Count ----------")
-	print(f"Found {num_words} total words")
-	print("--------- Character Count -------")
-	for item in sorted_list:
-		print(f"{item["char"]}: {item["num"]}\n")
-	print("============= END ===============")
+    print_report(book_path, num_words, chars_sorted_list)
+
+def get_book_text(filepath):
+	with open(filepath) as f:
+		return f.read()
+
+def print_report(
+    book_path: str,
+    num_words: int,
+    chars_sorted_list: list[tuple[str, int]]
+) -> None:
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for char, count in chars_sorted_list:
+        if not char.isalpha():
+            continue
+        print(f"{char}: {count}")
+
+    print("============= END ===============")
 
 main()
